d45f6abb65aec22dc86cf24bc80ec4585bc8e6b6
App Academy Student <student@appacademy.io>
2014-10-03 00:27:46 -0700

    Final touches for first iteration

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -14,44 +14,46 @@ class Assignment < ActiveRecord::Base
     class_name: "Boat",
     foreign_key: :boat_id,
     primary_key: :id  )
+    
+  private
   
-  def validate_assignment
+    def validate_assignment
 
-    overlapping_timeslots = self.timeslot.overlapping_timeslots
+      overlapping_timeslots = self.timeslot.overlapping_timeslots
 
-    # if there are no overlapping timeslots, the assignment is valid
-    return true if overlapping_timeslots.empty?
+      # if there are no overlapping timeslots, the assignment is valid
+      return true if overlapping_timeslots.empty?
 
-    timeslots_with_shared_boats = []
+      timeslots_with_shared_boats = []
 
-    overlapping_timeslots.each do |timeslot|
+      overlapping_timeslots.each do |timeslot|
 
-      if timeslot.boats.include?(self.boat)
-        timeslots_with_shared_boats << timeslot
+        if timeslot.boats.include?(self.boat)
+          timeslots_with_shared_boats << timeslot
+        end
+
       end
 
+      # if none of the overlapping timeslots have been assigned the boat, the assignment is valid
+      return true if timeslots_with_shared_boats.empty?
+      puts "IN VALIDATE ASSIGNMENT: THERE ARE TIMESLOTS WITH THIS BOAT ASSIGNED"
+    
+      # return false if any of the timeslots that also use this boat cannot accomodate
+      # their current bookings without it, i.e. they are already using the boat
+    
+      timeslots_with_shared_boats.each do |timeslot|
+      
+        boats_arr = timeslot.boats
+        idx = boats_arr.index(self.boat)
+        new_boats_arr = boats_arr[0...idx] + boats_arr[idx + 1..-1]
+        availability_without_boat = Timeslot.availability(timeslot.bookings.map(&:size), new_boats_arr.map(&:capacity))
+      
+        return false if !availability_without_boat
+      
+      end
+    
+      true
     end
-
-    # if none of the overlapping timeslots need to use the boat, the assignment is valid
-    return true if timeslots_with_shared_boats.empty?
-    puts "IN VALIDATE ASSIGNMENT: THERE ARE TIMESLOTS WITH THIS BOAT ASSIGNED"
-    
-    # return false if any of the timeslots that also use this boat cannot accomodate
-    # their current bookings without it, i.e. they are already using the boat
-    
-    timeslots_with_shared_boats.each do |timeslot|
-      
-      boats_arr = timeslot.boats
-      idx = boats_arr.index(self.boat)
-      new_boats_arr = boats_arr[0...idx] + boats_arr[idx + 1..-1]
-      availability_without_boat = Timeslot.availability(timeslot.bookings.map(&:size), new_boats_arr.map(&:capacity))
-      
-      return false if !availability_without_boat
-      
-    end
-    
-    true
-  end
   
 
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,122 +4,132 @@ class Booking < ActiveRecord::Base
   before_save :check_availability
   before_save :validate_booking
   
-
   belongs_to(
     :timeslot,
     class_name: "Timeslot",
     foreign_key: :timeslot_id,
     primary_key: :id  )
-  
-  # has_many :boats, through: :timeslot, source: :boats
-  
+
   private
 
-  def check_availability
-    return true if self.size <= self.timeslot.availability
-    false
-  end
-  
-  def validate_booking
-    
-    # return false right away if the booking size is larger than the largest possible availability.
-    return false unless check_availability
-    puts "AVAILABILITY CHECKS OUT"
-    
-    
-    # after checking the availability, if there are no overlapping timeslots, you're in the clear.
-    overlapping_timeslots = self.timeslot.overlapping_timeslots
-    return true if overlapping_timeslots.empty?
-    puts "THERE ARE OVERLAPPING TIMESLOTS"
-    
-    
-    # if there are overlapping timeslots but no shared boats, you're golden.
-    shared_boats = self.timeslot.shared_boats
-    return true if shared_boats.empty?
-    puts "THERE ARE SHARED BOATS"
-    
-    
-    # check to see whether the boats, excluding those that are shared, can accomodate the request.
-    uniq_capacities = self.timeslot.boats.map(&:capacity)
-    shared_capacities = shared_boats.map(&:capacity)
-    
-    shared_capacities.each do |shared_capacity|
-      # delete the first instance of each shared capacitiy
-      uniq_capacities.delete_at(uniq_capacities.index(shared_capacity))
+    def check_availability
+      return true if self.size <= self.timeslot.availability
+      false
     end
+  
+    def validate_booking
     
-    # timeslot.bookings shouldn't yet include this booking
-    bookings = self.timeslot.bookings.map(&:size)
-    uniq_availability = Timeslot.availability(bookings, uniq_capacities)
+      # return false right away if the booking size is larger than the largest possible availability.
+      return false unless check_availability
+      puts "AVAILABILITY CHECKS OUT"
     
-    # this checks to see whether or not the request could be accomodated without the shared boats.
-    return true if (uniq_availability && uniq_availability >= self.size)
-    puts "REQUEST CANNOT BE ACCODMODATED WITHOUT SHARED BOATS"
     
-    # the request requires one or more of the shared boats, 
-    # the following code is meant to choose the optimal combination, i.e.
-    # that which uses the fewest boats and has the least open space after the new booking:
+      # after checking the availability, if there are no overlapping timeslots, you're in the clear.
+      overlapping_timeslots = self.timeslot.overlapping_timeslots
+      return true if overlapping_timeslots.empty?
+      puts "THERE ARE OVERLAPPING TIMESLOTS"
     
-    shared_subs = subsets(shared_boats)
-    new_bookings = bookings + [self.size]
-
-    selected_sub = []
     
-    shared_subs.each do |sub|
+      # if there are overlapping timeslots but no shared boats, you're golden.
+      shared_boats = self.timeslot.shared_boats
+      return true if shared_boats.empty?
+      puts "THERE ARE SHARED BOATS"
+    
+    
+      # check to see whether the boats, excluding those that are shared, can accomodate the request.
+      uniq_capacities = self.timeslot.boats.map(&:capacity)
+      shared_capacities = shared_boats.map(&:capacity)
+    
+      shared_capacities.each do |shared_capacity|
+        # delete the first instance of each shared capacitiy
+        uniq_capacities.delete_at(uniq_capacities.index(shared_capacity))
+      end
+    
+      # timeslot.bookings shouldn't yet include this booking
+      bookings = self.timeslot.bookings.map(&:size)
+      uniq_availability = Timeslot.availability(bookings, uniq_capacities)
+    
+      # this checks to see whether or not the request could be accomodated without the shared boats.
+      return true if (uniq_availability && uniq_availability >= self.size)
+      puts "REQUEST CANNOT BE ACCODMODATED WITHOUT SHARED BOATS"
       
+      # there is a special case when there is more than one shared boat and each one has capacity larger than the request? 
+      
+    
+      # the request requires one or more of the shared boats, 
+      # the following code is meant to choose the optimal combination, i.e.
+      # that which uses the fewest boats and has the least open space after the new booking:
+    
+      shared_subs = subsets(shared_boats)
+      new_bookings = bookings + [self.size]
+      p "new_bookings", new_bookings
+      p "shared_subs", shared_subs
+
+      selected_sub = []
       fewest_boats = nil
       smallest_leftover_availability = nil
-
-      new_capacities = uniq_capacities + sub.map(&:capacity)
-      current_availability = Timeslot.availability(bookings, new_capacities)
-
-      next unless current_availability >= self.size
-        
-        leftover_availability = Timeslot.availability(new_bookings, new_capacities)
-        
-        if !fewest_boats && !smallest_leftover_availability
-          fewest_boats = sub.length
-          smallest_leftover_availability = leftover_availability
-          selected_sub = sub
-          next
-        end
-        
-        if sub.length <= fewest_boats && leftover_availability < smallest_leftover_availability
-          fewest_boats = sub.length
-          smallest_leftover_availability = leftover_availability
-          selected_sub = sub
-        end
-        
-      end
-      
-      # we must now remove other assignments that use the selected shared boats
-      selected_sub_ids = selected_sub.map(&:id)
-      query_string = 'boat_id IN ( '
-      selected_sub_ids.length.times { query_string = query_string + '?, ' }
-      query_string = query_string[0...-2] + ' )'
-      
-      overlapping_timeslots.each do |timeslot|
-        
-        assignments = timeslot.assignments.where(query_string, *(selected_sub_ids))
-        assignments.each do |record|
-          record.destroy!
-        end
-        
-      end
     
-    true
-  end
+      shared_subs.each do |sub|
 
-  def subsets(arr)
+        new_capacities = uniq_capacities + sub.map(&:capacity)
+        current_availability = Timeslot.availability(bookings, new_capacities)
 
-    return [[]] if arr.empty?
-
-    val = arr[0]
-    subs = subsets(arr[1..-1])
-    new_subs = subs.map { |sub| sub + [val] }
-
-    subs.concat(new_subs) 
+        next unless current_availability >= self.size
+        
+          leftover_availability = Timeslot.availability(new_bookings, new_capacities)
+        
+          if !fewest_boats && !smallest_leftover_availability
+            p "first set"
+            fewest_boats = sub.length
+            smallest_leftover_availability = leftover_availability
+            selected_sub = sub
+            p "selected_sub", selected_sub
+            p "fewest_boats", fewest_boats
+            p "smallest_leftover_availability", smallest_leftover_availability
+            next
+          end
+        
+          if sub.length <= fewest_boats && leftover_availability < smallest_leftover_availability
+            p "changing values"
+            fewest_boats = sub.length
+            smallest_leftover_availability = leftover_availability
+            selected_sub = sub
+            p "selected_sub", selected_sub
+            p "fewest_boats", fewest_boats
+            p "smallest_leftover_availability", smallest_leftover_availability
+          end
+        
+        end
+      
+      
+        # remove other assignments that use the selected shared boats
+        selected_sub_ids = selected_sub.map(&:id)
+        p "SELECTED SUB:", selected_sub
+        query_string = 'boat_id IN ( '
+        selected_sub_ids.length.times { query_string = query_string + '?, ' }
+        query_string = query_string[0...-2] + ' )'
+      
+        overlapping_timeslots.each do |timeslot|
+        
+          assignments = timeslot.assignments.where(query_string, *(selected_sub_ids))
+          assignments.each do |record|
+            record.destroy!
+          end
+        
+        end
     
-  end
+      true
+    end
+
+    def subsets(arr)
+
+      return [[]] if arr.empty?
+
+      val = arr[0]
+      subs = subsets(arr[1..-1])
+      new_subs = subs.map { |sub| sub + [val] }
+
+      subs.concat(new_subs)
+    end
   
 end
